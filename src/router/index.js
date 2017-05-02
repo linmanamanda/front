bea29b90e403fd2929f8168946ebacd5c70d9188
Amanda Lin <linman@heqiauto.com>
2017-05-02 17:54:57 +0800

    试用iview组件库

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import layout from '../components/common/layout'
+import Login from '../components/login/Login'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'layout',
+      component: layout
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
